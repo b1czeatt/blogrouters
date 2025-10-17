31696bfa678ee5e0edd242ad66705359609fa444
b1czeatt <b1czeatt@vasvari.org>
2025-10-17 10:11:41 +0200

    asd

--- util/authentication.js
+++ util/authentication.js
@@ -1,2 +1,26 @@
+import * as User from '../data/user.js'
+function auth(req,res,next){
+    try{
+        const accessToken = req.headers.authorization
+        if (!accessToken) {
+            return res.status(401).json({message: 'Unathorized'})
+        }
+        const token = jwt.verify(accessToken.split(' ')[1],'secret_key')
+        const now = Math.floor(Date.now()/1000)
+        if (!token || token?.exp < now) {
+            return res.status(403).json({message: 'Access forbidden'})
+        }
+        const user = User.getUserById(token.id)
+        if (!user) {
+            return res.status(403).json({message: 'Access denied'})
+        }
+        req.userId = user.id
+        req.userEmail = user.email
+        next()
+    }
+    catch (err){
+        res.status(500).json({error: err.message});
+        }
+    }
 
-
+export default auth;
